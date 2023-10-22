cc17d330f3aa1b9db51e68088a7a271fa70a3c05
aniveshpandey <anivesh@live.com>
2023-10-22 09:15:02 +0530

    Added Express Rate Limiter

--- src/app.js
+++ src/app.js
@@ -3,11 +3,19 @@ const PORT = eval(process.env.PORT);
 const express = require('express');
 const { checkSchema, body } = require('express-validator');
 const app = express();
+const rateLimit = require('express-rate-limit');
 const { userSchema } = require('./etc/schema.js');
 const { registerUser, loginUser, verifyUser, getPreferences, putPreferences ,updateUserReadNews, updateUserFavoriteNews } = require('./user/user-controller.js');
 const { getUserNews, getReadNews, getFavoriteNews, searchNews} = require('./news/news-controller.js');
-app.use(express.json());
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  message: 'Too many requests.',
+});
+
+app.use(express.json());
+app.use(limiter);
 
 app.get('/', (req,res) =>{
   res.status(200).send("News Aggregator API");
